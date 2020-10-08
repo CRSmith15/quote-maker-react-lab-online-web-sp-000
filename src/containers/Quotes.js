d826bb5c3f933b74b7160696906b80835a574fde
Chance Smith <chancesmith97@hotmail.com>
2020-10-08 21:17:05 +0000

    Automatically backed up by Learn

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -7,6 +7,12 @@ import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes';
 class Quotes extends Component {
 
   render() {
+
+    const quotesList = this.props.quotes.map(quote => <QuoteCard key={quote.id} quote={quote}
+      removeQuote={this.props.removeQuote}
+      upvoteQuote={this.props.upvoteQuote}
+      downvoteQuote={this.props.downvoteQuote} />)
+
     return (
       <div>
         <hr />

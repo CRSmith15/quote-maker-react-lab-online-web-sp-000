86b94979945e15a38748cb124f46e28da4e8add6
Chance Smith <chancesmith97@hotmail.com>
2020-10-08 20:51:56 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,9 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+  }
 }
